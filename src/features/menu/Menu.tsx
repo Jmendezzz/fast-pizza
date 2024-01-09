5c21e5c597355183f2d8df41b4e4ff900fc1cb39
Juan Gerardo <jmendez1025@cue.edu.co>
2024-01-09 08:23:51 -0500

    feat: Loader and useNavigation implemented

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,5 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData() as Array<any>; // Temp
+  return <div>
+    {menu.map(pizza=> <MenuItem key={pizza.id} pizza={pizza} />)}
+  </div>
+}
+
+
+export async function loader(){
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
