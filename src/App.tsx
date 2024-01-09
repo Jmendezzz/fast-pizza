85f6483a55afb0ff0035a081cfe9d1d2fb8aae3e
Juan Gerardo <jmendez1025@cue.edu.co>
2024-01-09 09:33:25 -0500

    feat: React Router Actions implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, {loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder";
+import Order, {loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error"
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -17,6 +19,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        errorElement: <Error />,
         loader:menuLoader
       },
       {
@@ -26,10 +29,13 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction
       },
       {
-        path: "/oder/:id",
+        path: "/order/:orderId",
         element: <Order />,
+        errorElement: <Error />,
+        loader:orderLoader
       },
     ],
   },
