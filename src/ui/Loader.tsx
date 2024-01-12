caf40b9239e554f6c4e735ac6387cd0dc03f4441
Juan Gerardo <jmendez1025@cue.edu.co>
2024-01-12 14:43:49 -0500

    feat: Loader Styles implemented

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -1,8 +1,9 @@
-
 function Loader() {
   return (
-    <div className="loader"></div>
-  )
+    <div className="absolute bg-slate-300/20 backdrop-blur-[1.5px] inset-0 flex items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
