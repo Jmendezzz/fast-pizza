49db209bfcb0f6696eda10b3ed7b9cad45a395df
Juan Gerardo <jmendez1025@cue.edu.co>
2024-01-14 19:55:02 -0500

    feat: Cart item styles implemented

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -1,14 +1,16 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p>
         {quantity}&times; {name}
       </p>
-      <div>
+      <div className="flex justify-between items-center sm:gap-6">
         <p>{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -5,28 +5,32 @@ function Button({
   children,
   disabled,
   to,
-  type
+  type,
 }: {
   children: ReactNode;
   disabled?: boolean;
   to?: string;
-  type:string
+  type: 'primary' | 'small';
 }) {
-    const base =  "rounded-lg bg-yellow-400 font-semibold uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed"
+  const base =
+    "rounded-lg bg-yellow-400 font-semibold uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed";
 
-    const styles = {
-    primary: base +  'px-4 py-3 md:px-6 md:py-4 ',
-    small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs'
 
-    }
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4 ',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+  };
   if (to) {
-    return <Link to={to} className={styles[type]}>{children}</Link>;
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
   }
   return (
-    <button
-      disabled={disabled}
-      className={styles[type]}
-    >
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
